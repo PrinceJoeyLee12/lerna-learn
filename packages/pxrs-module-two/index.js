4c60b439b27418c76344367cdfacc656ec4be036
Prince Joey Lee <prince.joey.lee@accenture.com>
2021-07-21 16:00:21 +0800

    adding two packages test

---
+++ packages/pxrs-module-two/index.js
@@ -0,0 +1,5 @@
+console.log('Module 2')
+
+export default function Module1() {
+    return "This is from module 2"
+}
---
+++ packages/pxrs-module/index.js
@@ -0,0 +1,5 @@
+console.log('Module 1')
+
+export default function Module1() {
+    return "This is from module 1"
+}
