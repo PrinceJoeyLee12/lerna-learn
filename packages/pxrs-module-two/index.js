338093571a713902a0daab8334a625f7767623f2
Prince Joey Lee <prince.joey.lee@accenture.com>
2021-07-21 17:05:50 +0800

    Publish
     - client@0.1.2
     - pxrs-module-two@0.0.5
     - pxrs-module@0.0.4

--- packages/pxrs-module-two/index.js
+++ packages/pxrs-module-two/index.js
@@ -2,5 +2,6 @@ console.log('Module 2')
 
 export default function Module1() {
     console.log('this is a test for change log')
+    console.log('this is a test for change log')
     return "This is from module 2"
 }
